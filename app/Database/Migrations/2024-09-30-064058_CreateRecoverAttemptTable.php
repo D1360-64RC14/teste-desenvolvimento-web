b31262b3c5029ed189672c078dbe35abf49f2f73
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-09-30 03:44:52 -0300

    feat: create database tables to keep track of password recover attempts

---
+++ app/Database/Migrations/2024-09-30-064058_CreateRecoverAttemptTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateRecoverAttemptTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+            'code' => [
+                'type' => 'CHAR',
+                'constraint' => 6
+            ],
+            'ip_address' => [
+                'type' => 'VARCHAR',
+                'constraint' => 45
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('recover_attempt');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('recover_attempt');
+    }
+}
