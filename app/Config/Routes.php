70db581202185e64496c6eab71f60e8d9a13f47f
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-09-30 00:59:06 -0300

    feat: post signin route validating and saving to database

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,7 +8,10 @@ use CodeIgniter\Router\RouteCollection;
  */
 
 $routes->get('/login', [Auth::class, 'login']);
+
 $routes->get('/signin', [Auth::class, 'signin']);
+$routes->post('/signin', [Auth::class, 'postSignin']);
+
 $routes->get('/forgot-password', [Auth::class, 'forgotPassword']);
 
 $routes->get('/', 'Home::index');
--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,7 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\User;
 
 class Auth extends BaseController
 {
@@ -19,6 +19,30 @@ class Auth extends BaseController
         helper('form');
 
         return view('auth/signin');
+    }
+
+    public function postSignin()
+    {
+        $data = $this->request->getPost(['name', 'email', 'password', 'verifyPassword']);
+
+        $userModel = model(User::class);
+
+        $userModel->setValidationRule('verifyPassword', 'required|matches[password]');
+        $userModel->setValidationMessage('verifyPassword', [
+            'required' => 'Confirme sua senha',
+            'matches' => 'As senhas devem ser iguais',
+        ]);
+
+        $valid = $userModel->save($data);
+
+        if (! $valid) {
+            $errors = $userModel->errors();
+            return redirect()->back()->withInput()->with('errors', $errors);
+        }
+
+        return redirect()->to('/login')->with('successes', [
+            'Conta criada com sucesso! Por favor realize o login',
+        ]);
     }
 
     public function forgotPassword()
