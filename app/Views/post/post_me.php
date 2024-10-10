48bbb3489975de789bd64f1891343a24023acb52
Diego Garcia <gdiego416@gmail.com>
2024-10-10 20:13:10 -0300

    feat: place dropdown button in post card view

--- app/Views/post/post_me.php
+++ app/Views/post/post_me.php
@@ -7,16 +7,35 @@
     <div class="card mt-3" id="<?= esc('post-' . $post['id'], 'attr') ?>">
         <div class="card-body">
             <h4 class="d-flex align-items-center">
-                <span class="h5 flex-grow-1"><?= esc($post['title']) ?></span>
-                <span class="h6 text-secondary ms-auto">
+                <span class="h5 flex-grow-1 mb-0"><?= esc($post['title']) ?></span>
+                <span class="h6 text-secondary mb-0">
                     <?= esc($user['email']) ?>
                 </span>
+                <div class="dropdown text-secondary ms-1">
+                    <button class="btn btn-sm dropdow-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-three-dots-vertical"></i>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="<?= esc('/post/' . $post['id'] . '/edit', 'attr') ?>">Editar</a>
+                        </li>
+                        <li>
+                            <a
+                                class="dropdown-item text-danger"
+                                href="<?= esc('/post/' . $post['id'] . '/delete', 'attr') ?>">Excluir</a>
+                        </li>
+                    </ul>
+                </div>
             </h4>
             <p><?= esc($post['body']) ?></p>
 
-            <div class="text-center">
-                <img class="img-thumbnail" src="<?= esc($post['image_url'], 'attr') ?>">
-            </div>
+            <?php if (!empty($post['image_url'])) : ?>
+                <div class="text-center">
+                    <img class="img-thumbnail" src="<?= esc($post['image_url'], 'attr') ?>">
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
--- app/Views/post/post_other.php
+++ app/Views/post/post_other.php
@@ -7,16 +7,18 @@
     <div class="card mt-3" id="<?= esc('post-' . $post['id'], 'attr') ?>">
         <div class="card-body">
             <h4 class="d-flex align-items-center">
-                <span class="h5 flex-grow-1"><?= esc($post['title']) ?></span>
-                <span class="h6 text-secondary ms-auto">
+                <span class="h5 flex-grow-1 mb-0"><?= esc($post['title']) ?></span>
+                <span class="h6 text-secondary mb-0">
                     <?= esc($user['email']) ?>
                 </span>
             </h4>
             <p><?= esc($post['body']) ?></p>
 
-            <div class="text-center">
-                <img class="img-thumbnail" src="<?= esc($post['image_url'], 'attr') ?>">
-            </div>
+            <?php if (!empty($post['image_url'])) : ?>
+                <div class="text-center">
+                    <img class="img-thumbnail" src="<?= esc($post['image_url'], 'attr') ?>">
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
