26d482b1ca6284dbe810b707bcaef29dbbfd2aa9
Diego Garcia <gdiego416@gmail.com>
2024-10-10 19:42:56 -0300

    feat: make title anchor wrap the entire article card

--- app/Views/home/post_other.php
+++ app/Views/home/post_other.php
@@ -1,13 +1,15 @@
-<article class="card" id="<?= esc('post-' . $postWithUser['id'], 'attr') ?>">
-    <div class="card-body">
-        <h4 class="d-flex align-items-center">
-            <a class="h5 flex-grow-1" href="<?= esc('/post/' . $postWithUser['id'], 'attr') ?>">
-                <?= esc($postWithUser['title']) ?>
-            </a>
-            <span class="h6 text-secondary ms-auto">
-                <?= esc($postWithUser['email']) ?>
-            </span>
-        </h4>
-        <p><?= esc($postWithUser['body']) ?></p>
-    </div>
-</article>
+<a class="text-decoration-none" href="<?= esc('/post/' . $postWithUser['id'], 'attr') ?>">
+    <article class="card" id="<?= esc('post-' . $postWithUser['id'], 'attr') ?>">
+        <div class="card-body">
+            <h4 class="d-flex align-items-center">
+                <span class="h5 flex-grow-1">
+                    <?= esc($postWithUser['title']) ?>
+                </span>
+                <span class="h6 text-secondary ms-auto">
+                    <?= esc($postWithUser['email']) ?>
+                </span>
+            </h4>
+            <p><?= esc($postWithUser['body']) ?></p>
+        </div>
+    </article>
+</a>
